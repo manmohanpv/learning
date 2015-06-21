2a624e77198819fb792741c876926ca3220898aa
manmohanpv <manmohanpv@gmail.com>
2015-06-21 12:27:36 -0700

    improve working memory

--- memory.py
+++ memory.py
@@ -5,6 +5,4 @@ x = int (input ('how many numbers you wanna try:'))
 
 for y in range(0,x):
     a = math.floor( (random.random () * 10))
-    print (a,end='')
-    print (' ',end='')    
-
+    print (a,' ',end='')
